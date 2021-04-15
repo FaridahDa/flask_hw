30a5965b8abcc303fd510c3d8ebbd1b9bebfddc8
DumisileM <dumisilem@aol.com>
2021-04-15 21:16:18 +0100

    in-class activity

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 from application import app
+app.config['SECRET_KEY']='TEAMD'
+
 if __name__ == "__main__":
     app.run(debug=True)
--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template,request
 
+from application.forms import BasicForm
 from application import app
 
 
@@ -14,4 +15,29 @@ def template_base(name):
 
 @app.route('/about_us')
 def about_us():
-    return render_template('about.html')
+    return render_template('about.html', title='About Us')
+
+@app.route('/favourites')
+def favourites():
+    data = ['food', 'cake', 'elephant', 'rainbow']
+    return render_template('favourites.html',data=data)
+
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    error = ""
+    form = BasicForm()
+
+    if request.method == 'POST':
+        first_name = form.first_name.date
+        last_name = form.last_name.data
+
+        if len(first_name) == 0 or len(last_name) == 0:
+            error = "Please supply both first and last name"
+        else:
+
+            return "Thank you!"
+
+    return render_template("register.html", form=form, message=error)
+
